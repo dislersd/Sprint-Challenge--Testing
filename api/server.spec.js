186418fac11f6c015f8a7665ca354b84e44c2c65
dislersd <dislersd@gmail.com>
2019-04-12 09:29:20 -0700

    wrote tests for get and post endpoints

---
+++ api/server.spec.js
@@ -0,0 +1,58 @@
+const request = require("supertest");
+const server = require("./server.js");
+
+describe("server", () => {
+  describe("GET /", () => {
+    it("responds 200 OK", async () => {
+      const res = await request(server).get("/");
+      expect(res.status).toBe(200);
+    });
+    it("responds with text", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toBe("text/html");
+    });
+  });
+  describe("POST /games", () => {
+    it("responds 201 with new post", async () => {
+      const newGame = { title: "League", genre: "Computer", releaseYear: 2005 };
+      const res = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(res.status).toBe(201);
+    });
+    it("responds 422 if post info is incomplete", async () => {
+      const newGame = { title: "League", releaseYear: 2005 };
+      const res = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(res.status).toBe(201);
+    });
+    it("returns new post after posting", async () => {
+      const newGame = { title: "League", genre: "Computer", releaseYear: 2005 };
+      const res = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(res.type).toBe("object");
+    });
+  });
+  describe('GET /games', () => {
+    it('Returns status 200', async () => {
+      const res = await request(server).get('/games');
+      expect(res.status).toBe(200)
+    });
+    it('Return empty array if no games', async () => {
+      const res = await request(server).get('/games')
+      expect(res.data).toEqual('[]')
+    });
+    it('Returns array of games', async () => {
+      const res = await request(server).get('/games');
+      expect(res.type).toBe('array')
+    });
+  });
+  describe("DELETE", () => {
+    it("responds 200", async () => {
+      const res = await request(server).delete("/shoes/:id");
+      expect(res.status).toBe(200);
+    });
+  });
+});
