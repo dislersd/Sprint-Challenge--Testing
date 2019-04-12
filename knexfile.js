7dd6fd760b16289055d5b3a954f4d580206f5b73
dislersd <dislersd@gmail.com>
2019-04-12 08:28:27 -0700

    scaffolding project - set up knexfile

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// if no DATABASE_URL env variable use this object
+const localPg = {
+  host: "localhost",
+  database: "games",
+  user: "student",
+  password: "hired"
+};
+const productionDbConnectionString = process.env.DATABASE_URL || localPg;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/games.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+  // Production is for hosting
+  production: {
+    client: "pg",
+    connection: productionDbConnectionString, // could be object or string
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+};
