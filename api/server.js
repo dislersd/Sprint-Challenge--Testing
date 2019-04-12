3597b28e1fc5e2a62649066e2315a3abd96d1684
dislersd <dislersd@gmail.com>
2019-04-12 08:43:30 -0700

    built new games table with knex

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const server = express();
-//const gamesRouter = require("../games-resource/games-router.js");
+const gamesRouter = require("../games-resource/games-router.js");
 
 server.use(express.json());
-//server.use("/games", gamesRouter);
+server.use("/games", gamesRouter);
 
 server.get("/", async (req, res) => {
   res.status(200).send("Jello World");
